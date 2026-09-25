be58d76aa64c5a31a6b206c86dbf84b0f5b36296
Rakesh Reddy <chittemrakesh@karunya.edu.in>
2026-09-25 09:51:53 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+
+        int profit = 0;
+        int maxprofit = 0;
+
+        int min = prices[0];
+
+        for(int i=0;i<prices.length;i++) {
+
+            min = Math.min(min,prices[i]);
+
+            profit = prices[i] - min;
+
+            maxprofit = Math.max(maxprofit, profit);
+
+
+        }
+
+
+        return maxprofit;
+
+        
+        
+    }
+}
